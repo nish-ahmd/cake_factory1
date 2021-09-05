d4f0d146ac0206ae26a1fb6ae64e238991082a19
ArzilRiyaj <arzilnishad@gmail.com>
2021-09-05 23:59:21 +0530

    commit

---
+++ allcake.php
@@ -0,0 +1,52 @@
+<?php
+
+include_once("header.php");
+?>
+
+
+
+
+
+
+<div>
+<img src="images/bg.jpg">
+</div>
+<!-- --------------------------------------------------------------------------------------------------------- -->
+
+
+
+
+<div class="container-fluid my-5">
+<div class="row">
+<div class="col-sm-2">
+
+qqsqs
+</div>
+<div class="col-sm-10">
+<div class="card text-center  " style="width: 18rem;"   >
+  <img class="card-img-top" src="images/bg.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   
+    <a href="#"  class="btn btn-primary downbtn ">Go somewhere</a>
+
+  </div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+
+
+
+
+
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<?php
+
+include_once("footer.php");
+?>
--- first.php
+++ first.php
@@ -44,26 +44,45 @@ include_once("header.php")
 
 <!-- /Image carosal -->
 
+<!-- 
+<div class="container-fluid home-icon-box"> -->
 
-<div class="container-fluid home-icon-box">
-
-  <div class=" border border-2 home-icon-box vc_custom ">
+  <!-- <div class=" border border-2 home-icon-box vc_custom "> -->
     <div class="row">
       <div class="col-sm-4">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
+        
+        
+  <div class="row ">
+    <div class="col-4">
+    <img src="images/iconbox1.jpg " class="rounded " alt="...">
+    </div>
+    <div class="col-8">
+    <h5 class="card-title">Special title treatment</h5>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    
+ 
+  </div>
+</div>
+
           </div>
         </div>
       </div>
       <div class="col-sm-4">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
+          <div class="row ">
+    <div class="col-4">
+    <img src="images/iconbox1.jpg " class="rounded " alt="...">
+    </div>
+    <div class="col-8">
+    <h5 class="card-title">Special title treatment</h5>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    
+ 
+  </div>
+</div>
           </div>
         </div>
       </div>
@@ -71,9 +90,17 @@ include_once("header.php")
       <div class="col-sm-4">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+          <div class="row ">
+    <div class="col-4  ">
+    <img src="images/iconbox1.jpg " class="rounded " alt="...">
+    </div>
+    <div class="col-8">
+    <h5 class="">Special title treatment</h5>
+            <p class="">With supporting text below as a natural lead-in to additional content.</p>
+    
+ 
+  </div>
+</div>
           </div>
         </div>
       </div>
@@ -86,103 +113,134 @@ include_once("header.php")
 
 <!-- -------------------------------------------------------------------------------------------------------------- -->
 <section>
-  <div class="container-fluid my-5">
+  <div class="container-fluid  my-5" >
 
 
 
-  <div class=" text-center heading-section ftco-animate">
-          	<span class="subheading">Services</span>
-            <h2 class="mb-3">Our Latest Services</h2>
-          </div>
-        </div>
-    <div class="row">
+    <div class=" text-center heading-section ftco-animate">
+      <span class="subheading">Services</span>
+      <h2 class="mb-3">Our Latest Services</h2>
+    </div>
+  
+
+  <div class="row">
+
 
     <div class="owl-carousel owl-theme">
-    <div class="item">
+      <div class="item">
 
 
-    <div class="card">
+        <div class="card">
 
-<img src="images/bg.jpg" class="card-img-top">
+          <img src="images/bg.jpg" class="card-img-top">
 
-<duv class="class-body">
-
-
-wdqwd
-</div>
-</div>
-<div class="item">
+          <duv class="class-body">
 
 
-<div class="card">
-
-<img src="images/bg.jpg" class="card-img-top">
-
-<duv class="class-body">
-
-
-wdqwd
-</div>
-</div>
-<div class="item">
+            wdqwd
+        </div>
+      </div>
+      <div class="item">
 
 
-<div class="card">
+        <div class="card">
 
-<img src="images/bg.jpg" class="card-img-top">
+          <img src="images/bg.jpg" class="card-img-top">
 
-<duv class="class-body">
-
-
-wdqwd
-</div>
-</div>
-<div class="item">
+          <duv class="class-body">
 
 
-<div class="card">
-
-<img src="images/bg.jpg" class="card-img-top">
-
-<duv class="class-body">
-
-
-wdqwd
-</div>
-</div>    <div class="item">
+            wdqwd
+        </div>
+      </div>
+      <div class="item">
 
 
-<div class="card">
+        <div class="card">
 
-<img src="images/bg.jpg" class="card-img-top">
+          <img src="images/bg.jpg" class="card-img-top">
 
-<duv class="class-body">
-
-
-wdqwd
-</div>
-</div>    <div class="item">
+          <duv class="class-body">
 
 
-    <div class="card">
-
-<img src="images/bg.jpg" class="card-img-top">
-
-<duv class="class-body">
-
-
-wdqwd
-</div>
-</div>
+            wdqwd
+        </div>
+      </div>
+      <div class="item">
 
 
-</div>
+        <div class="card">
+
+          <img src="images/bg.jpg" class="card-img-top">
+
+          <duv class="class-body">
+
+
+            wdqwd
+        </div>
+      </div>
+      <div class="item">
+
+
+        <div class="card">
+
+          <img src="images/bg.jpg" class="card-img-top">
+
+          <duv class="class-body">
+
+
+            wdqwd
+        </div>
+      </div>
+      <div class="item">
+
+
+        <div class="card">
+
+          <img src="images/bg.jpg" class="card-img-top">
+
+          <duv class="class-body">
+
+
+            wdqwd
+        </div>
+      </div>
 
 
     </div>
 
+
   </div>
+
+ 
 </section>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+
+
+
+
+<div class="container-fluid my-5">
+<div class=" text-center heading-section ftco-animate">
+      <span class="subheading">Services</span>
+      <h2 class="mb-3">Our Latest Services</h2>
+    </div>
+
+<div class="row featurette my-5">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+
+
+</div>
 <!-- ====================================================================================================================== -->
 <?php
 
