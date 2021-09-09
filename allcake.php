993eb8fe7d2408a646429136ada47c6047d2f1fe
ArzilRiyaj <arzilnishad@gmail.com>
2021-09-09 20:49:20 +0530

    insert done

--- allcake.php
+++ allcake.php
@@ -1,6 +1,14 @@
 <?php
 
 include_once("header.php");
+include_once "cake.php";
+
+$cake1 = new cake();
+
+      $q =$cake1->get_all_cake();
+
+      // print_r($q);
+
 ?>
 
 
@@ -18,26 +26,35 @@ include_once("header.php");
 
 <div class="container-fluid my-5">
 <div class="row">
-<div class="col-sm-2">
+<!-- <div class="col-sm-2">
 
 qqsqs
 </div>
-<div class="col-sm-10">
+
+<div class="col-sm-10"> -->
+
+<?php foreach($q as $item):  ?>
+
+
+<div class="col-sm-4">
 <div class="card text-center  " style="width: 18rem;"   >
   <img class="card-img-top" src="images/bg.jpg" alt="Card image cap">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title"><?=$item->cake_name ?></h5>
+    <p class="card-text"><?=$item->cake_discription ?></p>
    
     <a href="#"  class="btn btn-primary downbtn ">Go somewhere</a>
 
-  </div>
-</div>
-
-</div>
 
 </div>
 </div>
+
+</div>
+<?php endforeach ;  ?>
+</div>
+
+<!-- </div>
+</div> -->
 
 
 
